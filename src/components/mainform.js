c26fed27010235456c4ee15f55e034a8143174ff
Alyssa Cooper <alyssarcoops@gmail.com>
2020-02-24 14:31:38 -0500

    Home page done

--- src/components/mainform.js
+++ src/components/mainform.js
@@ -1,112 +1,167 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default class MainForm extends React.Component {
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+
   
-  //potential save function
-  // constructor(props){
-  //   super(props)
-  //   this.onSave = this.onSave.bind(this)
-  // }
-  // onSave(values) {
-  //   this.props.onSaveBrew(values)
-      
-  // }
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(2, 0, 1),
+  },
+}));
 
-  render() {
-    return (
-      <React.Fragment>
-        {/* onSave={this.onSave}
-        render={({ handleSubmit, reset, submitting, pristine }) => ( */}
-      <Typography variant="h1" gutterBottom>
-       Unbrew
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="coffeeName"
-            name="coffeeName"
-            label="Coffee Name"
-            fullWidth
-            autoComplete="cofname"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="origin"
-            name="origin"
-            label="Origin"
-            fullWidth
-            autoComplete="ori"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="roaster"
-            name="roaster"
-            label="Coffee Roaster"
-            fullWidth
-            autoComplete="billing address-line1"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="brewMethod"
-            name="brewMethod"
-            label="Brew Method"
-            fullWidth
-            autoComplete="billing address-line2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="grindSize"
-            name="grindSize"
-            label="Grind Size"
-            fullWidth
-            autoComplete="billing address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="time" name="time" label="Time" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="water"
-            name="water"
-            label="Water Amount"
-            fullWidth
-            autoComplete="billing postal-code"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="temperature"
-            name="temperature"
-            label="Temperature"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-        <TextField
-          id="standard-textarea"
-          label="Steps"
-          placeholder="Placeholder"
-          multiline
-          fullWidth
-        />
-        </Grid>
-        <Grid item xs={12}>
-        <Button variant="contained" color="secondary" onClick={() => { alert("✔️ This works on every component!"); }} >
+export default function MainForm() {
+  
+  // constructor(props, context) {
+    //   super(props, context);
+    //   this.state = {
+      //     coffeeName: [],
+      //     origin: "",
+      //     roaster: "",
+      //     brewMethod:"",
+      //     grindSize:"",
+      //     time:"",
+      //     water:"",
+      //     temperature:"",
+      //     steps:""
+      //   };
+      // }
+      
+      // onSave(e) {
+        //   // e.preventDefault();
+        //   const coffee = {
+          //     coffeeName: this.state.coffeeName,
+          //   }
+          //   axios.post('http://localhost:9000/account', coffee)
+          //   .then(res => console.log(res.data));
+          // }
+          
+          // componentDidMount() {
+            //   this.onSave();
+            // }
+            
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="s">
+      <CssBaseline />
+      <div className={classes.paper}>
+        {/* <Avatar className={classes.avatar}>
+        </Avatar> */}
+        <Typography component="h1" variant="h5">
+        Create a New Brew
+        </Typography>
+        <p> </p>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="coffeeName"
+                variant="outlined"
+                required
+                fullWidth
+                id="coffeeName"
+                label="Coffee"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="origin"
+                label="Coffee Origin"
+                name="origin"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="roaster"
+                label="Roaster"
+                name="roaster"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="brewmethod"
+                label="Brew Method"
+                id="brewmethod"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="grindsize"
+                label="Grind Size"
+                id="grindsize"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="wateramount"
+                label="Water Amount"
+                id="wateramount"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="temperature"
+                label="Temperature"
+                id="temperature"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="steps"
+                label="Detailed Steps"
+                id="steps"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="save"
+            // fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
             Save
-        </Button>
-        </Grid>
-      </Grid>
-    </React.Fragment>
-    );
-  }
+          </Button>
+          <Grid container justify="center">
+            <Grid item>
+              {/* <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link> */}
+            </Grid>
+          </Grid>
+      </div>
+    </Container>
+  );
+
 }
